f51e0448617ae79f25bb7513b3eaaff6297d7c30
Pablo Romo <djromo2296@gmail.com>
2023-12-19 19:00:56 -0600

    agregue dependencias del archivo JSON

---
+++ src/services/records/index.js
@@ -0,0 +1,13 @@
+class Records {
+    constructor() {
+        this.records = []
+    }
+    async getRecords(){
+        try{
+            const queryString = ``;
+            
+        }catch (error){
+            throw new Error(error);
+        }
+    }
+}
