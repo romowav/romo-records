8d96dcc6a7ee1417d7b33547f01052ccc8872c25
Pablo Romo <djromo2296@gmail.com>
2023-12-20 00:13:37 -0600

    archivo env esta funcionando

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const { config } = require('./config/index')
+const port = config.port;
 
-// METODOS / ENDPOINTS
 
-app.get('/holi', (req, res) => {
-    res.send('si esta jalando');
-})
+// Con esto mi API ya puede consumir archivos de tipo .json
+app.use(express.json());
+
+// Esto nos ayuda para poner filtros de quien puede acceder a la API, revisar clase del 12/14/2023 Backend 1
+app.use(cors());
 
 // SERVIDOR
 app.listen(port, () => {
